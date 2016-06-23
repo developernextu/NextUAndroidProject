4f4347a50021ba6f7e2dc5735c51e27fc3358e42
Jason <jasonarangozapata@gmail.com>
2016-06-23 10:47:01 -0500

    Nuestro primer commit desde Android Studio...

---
+++ app/src/main/java/com/example/nextu/nextuandroidproject/NextU.java
@@ -0,0 +1,13 @@
+package com.example.nextu.nextuandroidproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/* Add all the references on this file */
+public class NextU extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_u);
+        /* This is our main class for our NextU Android Studio Project*/
+    }
+}
